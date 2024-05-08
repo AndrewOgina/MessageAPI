56d5ef1543d1dd4a298f8ac452d98f4ea09e1570
primal-drew <oginaandrew05@gmail.com>
2024-05-08 11:49:39 +0300

    Client: Created the client header file.

---
+++ Clients/include/client.h
@@ -0,0 +1,42 @@
+#ifndef CLIENT_H
+#define CLIENT_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#define MAXLEN 4096
+#define PORT 8088
+#define SA struct sockaddr
+
+/**
+ * @brief Creates a socket and returns the socket file descriptor.
+ * @param serverType - SOCK_STREAM(for TCP) or SOCK_DGRAM(for Datagram).
+ * @return An integer which is the socket file descriptor.
+ * @note Ipv4 only.
+*/
+int getSocket(int serverType);
+
+/**
+ * @brief Fills a sockaddr_in  struct and returns it.
+ * @param port - The port the server runs on.
+ * @return A sockaddr_in struct
+ */
+struct sockaddr_in get_serverAddr(int port,char address);
+
+/**
+ * @brief Connects to a TCP server.
+ * @param serverFD - The socket file descriptor for the server.
+ * @param serverAddr -  A sockaddr_in struct containing the server address info.
+ * @return void.
+ * @note The serverAddr struct must be filled. 
+*/
+void connectTCP(int serverFD,struct sockaddr_in serverAddr);
+
+
+#endif
