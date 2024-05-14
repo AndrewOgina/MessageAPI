8ea9fce291d16ee947657edf4baf39ec7b00414f
AndrewOgina <muregiandrew@gmail.com>
2024-05-14 23:34:05 +0300

    Server: Started over taking a different approach.

--- Server/src/server.c
+++ Server/src/server.c
@@ -1,173 +1,204 @@
 #include "../include/server.h"
-#define END_TAG "\033[0m"
-
-typedef enum Format
-{
-    BOLD,
-    UNDERLINE,
-    BOLD_UNDERLINE,
-    BOLD_RED
-}Format;
 
 /**
- * @brief Takes a text then formats.
- * @param text - The text to be formatted.
- * @param format - BOLD / UNDERLINE / BOLD_UNDERLINE / BOLD_RED.
- * @return A char* containing the formatted text.
- * @warning FREE the char* after use
+ * @brief Checks if a function encountered an error.
+ * @param return_val - The value returned by a function.
+ * @param error_val - The value returned by unsuccessful function call.
+ * @param error_message - The message printed incase of an error.
+ * @return The value returned upon successful function call.
  */
-char *format_text(char *text, Format format)
-{
-    int prefix_length = 15;
-    int end_tag_len = strlen(END_TAG);
-    int textLen = strlen(text);
-    char prefix[prefix_length];
-    char *formatted = (char *)malloc((prefix_length + end_tag_len + textLen + 1)); // +1 for the null terminator.
-
-    switch (format)
-    {
-    case BOLD:
-        strcpy(prefix,"\033[1m");
-        break;
-    case UNDERLINE:
-        strcpy(prefix,"\033[4m");
-        break;
-    case BOLD_UNDERLINE:
-        strcpy(prefix,"\033[1;4m");
-        break;
-    case BOLD_RED:
-        strcpy(prefix,"\033[1;32;40m");
-    } 
-    
-    strncpy(formatted, prefix, prefix_length);
-    strncat(formatted, text, textLen);
-    strncat(formatted, END_TAG, end_tag_len);
-    return formatted;
-}
+int check_error(int return_val, int error_val, char *error_message);
 
 /**
- * @brief Error handling.
- * @param returnVal - The value returned by a function.
- * @param errVal - The value returned by unsuccessful function call.
- * @param errorMsg - The message printed incase of an error.
-*/
-int errHandle(int returnVal, int errVal, char *errorMsg)
+ * @brief Receives message from clients and broadcasts it to everyone!
+ * @param sender_fd The sender's file descriptor.
+ * @param epoll_fd -The epoll file descriptor.
+ * @param clients - An array of Client structs.
+ * @return void!
+ */
+void receive_and_broadcast(int sender_fd, int epoll_fd, struct Client *clients);
+
+/**
+ * @brief Retrieves the session username of connected clients.
+ * @param client_fd - The client's file descriptor.
+ * @param username_buffer - Buffer storing the username.
+ * @return void!
+ */
+void get_username(int client_fd, char *username_buffer);
+
+int check_error(int return_val, int error_val, char *error_message)
 {
-    if (returnVal == errVal)
+    if (return_val == error_val)
     {
-        perror(errorMsg);
+        perror(error_message);
         exit(EXIT_FAILURE);
     }
-    return returnVal;
+    return return_val;
 }
 
-int setup_server(int port, int sockType, int backlog)
+void get_username(int client_fd, char *username_buffer)
 {
-    int serverFD;
-    SA_IN serverAddr;
+    char welcome_message[] = "\033[1;31mWELCOME TO OTP.\033[0m\n"; //> The welcome message!
+    char login_feedback[] = "Login Successful!\n";                 //> Login  message feedback!
+    int username_size;                                             //> The username size.
+
+    // Sending welcome note!
+    check_error((send(client_fd, welcome_message, strlen(welcome_message), 0)), SOCK_ERROR, "Send: Welcome msg: Server side\n");
+
+    // Receiving the username!
+    check_error((username_size = recv(client_fd, username_buffer, USERNAME_SIZE - 1, 0)), SOCK_ERROR, "Receive: Username: Server side!");
+    username_buffer[username_size] = '\0';
+    fprintf(stdout, "Username: \033[1m%s\033[0m\n", username_buffer);
+
+    // Confirmatory message!
+    check_error((send(client_fd, login_feedback, strlen(login_feedback), 0)), SOCK_ERROR, "Send: successful login message failed to be sent!");
+}
+
+int setup_server(int port, int sock_type, int backlog)
+{
+    int server_fd;     //> The server's file descriptor!
+    SA_IN server_addr; //> The struct containing the address information of the server!
 
     // 1. Creating the socket.
 
-    errHandle((serverFD = socket(AF_INET, sockType, 0)), SOCK_ERROR, "Socket: Server side!\n");
+    check_error((server_fd = socket(AF_INET, sock_type, 0)), SOCK_ERROR, "Socket: Server side!\n");
     fprintf(stdout, "Socket created!\n");
-    
-    // 2. Filling the serverAddr struct.
 
-    serverAddr.sin_family = AF_INET;
-    serverAddr.sin_addr.s_addr = INADDR_ANY;
-    serverAddr.sin_port = htons(port);
+    // 2. Filling the server_addr struct.
+
+    server_addr.sin_family = AF_INET; //> Ipv4
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+    server_addr.sin_port = htons(port);
 
     // 3. Binding socket to address and port.
 
-    errHandle((bind(serverFD, (SA *)&serverAddr, sizeof(serverAddr))), SOCK_ERROR, "Bind: Server side!\n");
+    check_error((bind(server_fd, (SA *)&server_addr, sizeof(server_addr))), SOCK_ERROR, "Bind: Server side!\n");
     fprintf(stdout, "Socket binded to an address!\n");
 
     // 4. Listen for clients.
 
-    errHandle((listen(serverFD, backlog)), SOCK_ERROR, "Listen: Server side!\n");
+    check_error((listen(server_fd, backlog)), SOCK_ERROR, "Listen: Server side!\n");
     fprintf(stdout, "Listening...\n");
 
     // 5. Returning the server's file descriptor.
-    return serverFD;
+    return server_fd;
 }
 
-/**
- * @brief Requests the session username of connected clients.
- * @param clientFD - The client's file descriptor.
- * @return A formatted username.
- */
-void handleLogin(int clientFD, char *userName)
+void receive_and_broadcast(int sender_fd, int epoll_fd, struct Client *clients)
 {
-    int usernameSize;
-    // Prefixing some  messages!
+    int username_size;    //> Username size.
+    char message[MAXLEN]; //> Buffer to store the message.
+    ssize_t bytes_received;
+    bytes_received = recv(sender_fd, message, MAXLEN - 1, 0);
 
-    char welcomeMsg[25] = "WELCOME TO OTP.\n"; // Array should be large enough for bolding  and underlining tags!
+    if (bytes_received <= 0)
+    {
+        fprintf(stdout, "Client disconnected!\n");
+        epoll_ctl(epoll_fd, EPOLL_CTL_DEL, sender_fd, NULL);
 
-    // Bolding and underlining!
-    char* formatted_welcomeMsg = format_text(welcomeMsg,BOLD_UNDERLINE);
+        close(sender_fd);
+        for (int j = 0; j < MAX_CLIENTS; j++)
+        {
+            if (clients[j].client_fd == sender_fd)
+            {
+                clients[j].state = false;
+                break;
+            }
+        }
+    }
+    else
+    {
 
-    char loginMsg[32] = "You are logged in as:";
-    char* formatted_loginMsg =  format_text(loginMsg,BOLD);
-
-    // Requesting session username!
-
-    errHandle((send(clientFD, formatted_welcomeMsg, strlen(formatted_welcomeMsg), 0)), SOCK_ERROR, "Send: Welcome msg: Server side\n");
-    free(formatted_welcomeMsg);
-    // Receiving the username!
-
-    errHandle((usernameSize = recv(clientFD, userName, MAX_USERNAME_SIZE - 1, 0)), SOCK_ERROR, "Receive: Username: Server side!");
-    // Null terminating the username!
-
-    // A little bit of formatting.
-    char* formatted_userName = format_text(userName,BOLD_RED);
-
-    fprintf(stdout, "Username: %s\n", formatted_userName);
-
-    // Confirmatory message!
-    errHandle((send(clientFD, formatted_loginMsg, strlen(formatted_loginMsg), 0)), SOCK_ERROR, "Send: successful login message failed to be sent!");
-    free(formatted_loginMsg);
-    errHandle((send(clientFD, formatted_userName, strlen(formatted_userName), 0)), SOCK_ERROR, "Send: resending username failed to be sent!");
-    free(formatted_userName);
+        message[bytes_received] = '\0';
+        for (int i = 0; i < MAX_CLIENTS; i++)
+        {
+            if ((clients[i].state == ONLINE) && (clients[i].client_fd != sender_fd))
+            {
+                check_error((send(clients[i].client_fd, message, strlen(message), 0)), SOCK_ERROR, "send: broadcasting!");
+            }
+        }
+    }
 }
 
-struct Clients acceptConnections(int serverFD)
+void handle_connections(int server_fd)
 {
-    // Variable declarations!
-    Clients clients;
-    socklen_t clientAddrSize = sizeof(SA_IN);
-    SA_IN clientAddr;
-    int clientFD;
-    char clientIP[INET_ADDRSTRLEN];
-    char userName[MAX_USERNAME_SIZE + 10];
 
-    // Accepting new connections!
+    struct epoll_event event;            //> Struct holds new epoll events.
+    Client clients[MAX_CLIENTS];         //> Array to store clients.
+    socklen_t addr_size = sizeof(SA_IN); //> Client address size.
+    SA_IN client_addr;                   //> Struct holding client address information.
+    int client_fd, epoll_fd;             //> File descriptor.
+    int num_events;                      //> The number of fds available returned by epoll_wait.
 
-    errHandle((clientFD = accept(serverFD, (SA *)&clientAddr, &clientAddrSize)), SOCK_ERROR, "Accept: Server side!");
-    fprintf(stdout, "Accepting connections...\n");
+    // Creating an epoll instance
+    check_error((epoll_fd = epoll_create1(0)), SOCK_ERROR, "epoll_create1:server side!");
 
-    // Getting the Ip address 
-    inet_ntop(AF_INET, &clientAddr, clientIP, sizeof(clientIP));
-    fprintf(stdout, "Accepted connection from: %s...\n", clientIP);
+    // Adding server_fd to epoll
+    event.events = EPOLLIN;
+    event.data.fd = server_fd;
+    check_error((epoll_ctl(epoll_fd, EPOLL_CTL_ADD, server_fd, &event)), SOCK_ERROR, "epoll_ctl: server side!");
 
-    handleLogin(clientFD, userName);
+    // Set all clients to false
+    for (int i = 0; i < MAX_CLIENTS; i++)
+    {
+        clients[i].state = false;
+    }
 
-    clients.clientFD = clientFD;
-    strcpy(clients.userName, userName);
-    // Returning the client's file descriptor.
-    return clients;
+    // Infinite loop for accepting clients and sending and receiving messages!
+    while (1)
+    {
+        struct epoll_event events[MAX_CLIENTS];
+        check_error((num_events = epoll_wait(epoll_fd, events, MAX_CLIENTS, -1)), SOCK_ERROR, "epoll_wait: Server side!"); // -1 meaning indefinite timeout.
+
+        for (int i = 0; i < num_events; i++)
+        {
+            if (events[i].data.fd == server_fd)
+            {
+                if ((client_fd = accept(server_fd, (SA *)&client_addr, &addr_size)) == SOCK_ERROR)
+                {
+                    perror("accept: Server side!");
+                    continue;
+                }
+
+                // Add connected clients to epoll
+                event.events = EPOLLIN | EPOLLET;
+                event.data.fd = client_fd;
+
+                if ((epoll_ctl(epoll_fd, EPOLL_CTL_ADD, client_fd, &event)) == SOCK_ERROR)
+                {
+                    perror("epoll_ctl: Adding clients: Server side");
+                    continue;
+                }
+
+                int clientNo; // Number of connected clients
+
+                for (clientNo = 0; clientNo < MAX_CLIENTS; clientNo++)
+                {
+                    if (clients[clientNo].state == false)
+                    {
+                        char username[USERNAME_SIZE + 15];
+                        clients[clientNo].client_fd = client_fd;
+                        clients[clientNo].state = true;
+                        fprintf(stdout, "New client connected!\n");
+                        get_username(clients[clientNo].client_fd, username);
+                        strncpy(clients[clientNo].username, username, USERNAME_SIZE);
+                        break;
+                    }
+                }
+
+                if (clientNo == MAX_CLIENTS)
+                {
+                    fprintf(stdout, "Maximum number of clients attained!\n");
+                    close(client_fd);
+                }
+            }
+            else
+            {
+                int sender_fd = events[i].data.fd; //> The sender's file descriptor.
+                receive_and_broadcast(sender_fd, epoll_fd, clients);
+            }
+        }
+    }
 }
 
-void handleMessaging(struct Clients clients)
-{
-    char message[MAXLEN];
-    char* formatted_userName = format_text(clients.userName,BOLD_RED);
-    strcpy(message, formatted_userName);
-    strcat(message, "Hello World!");
-    char *newLine = strchr(message, '\n');
-    *newLine = ':';
-    send(clients.clientFD, message, strlen(message), 0);
-    free(formatted_userName);
-}
-
-// TODO Use the client struct to store connected client info
-// TODO Try to use threads / select to avoid non blocking code.
+// Note I am giving username prefixing responsibilities to the client.
