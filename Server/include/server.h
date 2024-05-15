dd1d8eef2923130e5a32581c0a9662c845851936
AndrewOgina <muregiandrew@gmail.com>
2024-05-15 17:26:40 +0300

    Server: Formatted the documented.

--- Server/include/server.h
+++ Server/include/server.h
@@ -22,41 +22,34 @@
 #define ONLINE true
 #define OFFLINE false
 
-
 /**
  * @brief The struct holding the client's information.
  * @param clientFD - The client's File descriptor.
- * @param userName - The username of the client. 
+ * @param userName - The username of the client.
  * @param state - Either ONLINE or OFFLINE.
-*/
+ */
 typedef struct Client
 {
-    int client_fd; //-> The client's File descriptor.
-    char username[USERNAME_SIZE]; //-> The username of the client. 
-    bool state; //-> Either ONLINE or OFFLINE.
-}Client;
+    int client_fd;                //-> The client's File descriptor.
+    char username[USERNAME_SIZE]; //-> The username of the client.
+    bool state;                   //-> Either ONLINE or OFFLINE.
+} Client;
 
 /**
- * @brief Sets up the server socket and listens for connections. 
+ * @brief Sets up the server socket and listens for connections.
  * @param port - The port number the server will run on.
  * @param sock_type - Either SOCK_STREAM (TCP server) or SOCK_DGRAM (UDP server).
  * @param backlog- The number of clients the server can listen at a time.
  * @return The server's file descriptor.
-*/
-int setup_server(int port,int sock_type,int backlog);
+ */
+int setup_server(int port, int sock_type, int backlog);
 
 /**
  * @brief Accepts new connections,stores their details and receives and broadcasts messages.
  * @param serverFD - The file descriptor the server is listening on.
  * @return void!
  * @note FOR TCP SERVERS & FOR BROADCASTS.
-*/
+ */
 void handle_connections(int serverFD);
-
-
-
-
-
-
 
 #endif
