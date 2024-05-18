1efc1bb7022abd3e6b2b1ee2bf6fd3247c3599dc
AndrewOgina <muregiandrew@gmail.com>
2024-05-18 14:03:52 +0300

    Client: Testing the new functions.

--- Clients/src/main.c
+++ Clients/src/main.c
@@ -1,30 +1,16 @@
 #include "../include/client.h"
 #include <pthread.h>
-#define PORT 8080
+#define PORT 8088
 
-
-int main(int argc,char* argv[])
+int main(int argc, char *argv[])
 {
-    int serverFD;
-    char message[MAXLEN];
-    struct sockaddr_in serverAddr;
-
-//@brief Checks whether the server IP address was provided at startup.
     if(argc != 2)
     {
-        fprintf(stderr,"Usage: ./bin/client <Server Ip address e.g 127.0.0.1>");
+        fprintf(stderr,"Provide the server address please!\n");
         exit(EXIT_FAILURE);
     }
-    fprintf(stdout,"Server address: %s\n",argv[1]);
-    serverFD = getSocket(AF_INET);
-    serverAddr = get_serverAddr(PORT,argv[1]);
-    connectTCP(serverFD,serverAddr);
+    int server_fd = connect_to_server(PORT, argv[1]);
 
-    while(1)
-    {
-        sendTCP(serverFD,message);
-    }
-    close(serverFD);
+    join_broadcast(&server_fd);
     
-    return 0;
 }
