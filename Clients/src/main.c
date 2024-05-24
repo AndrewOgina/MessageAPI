f7dd97c25cf1e10714d5f7caf31a2da60d997d74
AndrewOgina <muregiandrew@gmail.com>
2024-05-24 21:08:25 +0300

    client: Increased main parameters requirements.

--- Clients/src/main.c
+++ Clients/src/main.c
@@ -4,9 +4,9 @@
 
 int main(int argc, char *argv[])
 {
-    if(argc != 2)
+    if(argc != 3)
     {
-        fprintf(stderr,"Provide the server address please!\n");
+        fprintf(stderr,"Provide the: 1.Server address!\n             2.Your username!\n");
         exit(EXIT_FAILURE);
     }
     int server_fd = connect_to_server(PORT, argv[1]);
